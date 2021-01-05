f8c610011a9be3cda6f34fa855fe988e0a6c13ba
Eozef <eozefow@gmail.com>
2021-01-06 04:40:16 +1100

    folder restructured

---
+++ src/components/Directory.js
@@ -0,0 +1,60 @@
+import React from "react";
+import MenuItem from "./MenuItem";
+import "./Directory.scss";
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          title: "Dan A4",
+          imageUrl: "https://i.ibb.co/X4CV5qr/a4-960x.jpg",
+          size: "large",
+          id: 1,
+          linkUrl: "Dan",
+        },
+        {
+          title: "Ncase",
+          imageUrl: "https://i.ibb.co/yfKKchf/m1-960x.jpg",
+          size: "large",
+          id: 2,
+          linkUrl: "",
+        },
+        {
+          title: "MJOLNIR",
+          imageUrl: "https://i.ibb.co/hf87XbN/mr-960x.jpg",
+          size: "large",
+          id: 3,
+          linkUrl: "",
+        },
+        {
+          title: "FormD T1",
+          imageUrl: "https://i.ibb.co/mvGCgK3/hero-t1-960x.jpg",
+          size: "large",
+          id: 4,
+          linkUrl: "",
+        },
+        {
+          title: "louqe Ghost S1",
+          imageUrl: "https://i.ibb.co/mTMvNDR/g1-960x.jpg",
+          size: "large",
+          id: 5,
+          linkUrl: "",
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
---
+++ src/components/SignIn.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import "./SignIn.scss";
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I have an account</h2>
+        <span>Sign in with your email and password</span>
+        <form>
+          <input name="email" type="email" value={this.state.email} required />
+          <label>Email</label>
+          <input
+            name="email"
+            type="password"
+            value={this.state.password}
+            required
+          />
+          <label>Email</label>
+
+          <input type="submit" value="submit Form" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
